24d9d6a555076d99ce6f7a2d1072f2eec6b8e922
s1gn4r <signar85@hotmail.com>
2014-04-04 12:49:44 +0200

    Bootstrap template

--- Kode/index.php
+++ Kode/index.php
@@ -1,3 +1,10 @@
-<?php
-    phpinfo();
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+    </head>
+    <body>
+        <H1>Overskrift</h1>
+    </body>
+</html>
