b5a2a8a609afa1be8573095c19ce555cb083e8ef
s1gn4r <signar85@hotmail.com>
2014-04-14 12:05:39 +0200

    Changed menu file and icons

--- Kode/include/menu.php
+++ Kode/include/menu.php
@@ -1,5 +1,6 @@
 <ul class="main-menu">
-    <li><a href="index.php"><i class="fa fa-dashboard fa-fw"></i><span class="text">Dashboard</span></a></li>
+    <li><a href="index.php"><i class="fa fa-home fa-fw"></i><span class="text">Dashboard</span></a></li>
     <li><a href="customers.php"><i class="fa fa-users fa-fw"></i><span class="text">Customers</span></a></li>
     <li><a href="groups.php"><i class="fa fa-tags fa-fw"></i><span class="text">Groups</span></a></li>
+    <li><a href="ads.php"><i class="fa fa-picture-o fa-fw"></i><span class="text">Ads</span></a></li>
 </ul>
