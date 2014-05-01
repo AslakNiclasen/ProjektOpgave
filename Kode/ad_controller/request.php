24eae0b5de3122ecd2c5053bacac4ff3cd10682f
s1gn4r <signar85@hotmail.com>
2014-05-01 12:56:20 +0200

    file added

--- Kode/ad_controller/request.php
+++ Kode/ad_controller/request.php
@@ -1,7 +1,18 @@
 <?php
     require_once("connect.php");
+    require_once("show_ad.php");
     
-    echo "Du kom fra: ". $_SERVER["HTTP_REFERER"];
+    //echo "Du kom fra: ". $_SERVER["HTTP_REFERER"];
+
+    $referer = $_SERVER["HTTP_REFERER"];
+
+    if (substr($referer, 0, 6) == "http://") {
+
+    }
+
+    if (substr($referer, 0, 3) == "www.") {
+
+    }
 
     //$ads = $conn->query("SELECT * FROM ads WHERE id = '5'")->fetch_array();
     
